2c17381385fd9e999a8a3d6a4f0471e8c0829216
Michaelt293 <Michaelt293@gmail.com>
2015-12-24 15:53:09 +1100

    First commit

---
+++ changeKeys.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xmodmap -e "keycode 97 = Shift_L NoSymbol Shift_L"
+xmodmap -e "keycode 66 = Escape"
+xmodmap -e "keycode 132 = BackSpace"
+xmodmap -e "keycode 102 = BackSpace"
+xmodmap -e "keycode 100 = Return"
+
+
+
